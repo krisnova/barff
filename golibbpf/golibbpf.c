27fb5866fdce90fa20794ace21e067982c597c25
Kris Nova <kris@nivenly.com>
2019-12-19 19:49:05 -0800

    Updating comments for the C so folks can review
    Signed-off-by: Kris Nova <kris@nivenly.com>

--- golibbpf/golibbpf.c
+++ golibbpf/golibbpf.c
@@ -1,7 +1,5 @@
 #include <bpf/bpf.h>
 
 int load_bpf_object_by_filename(char *path) {
-
-
     return 1;
 }
--- golibbpf/golibbpf.h
+++ golibbpf/golibbpf.h
@@ -1,9 +1,7 @@
-
-
 /**
+ * So this would basically be a similar function to what is found in linux/samples
+ * https://github.com/torvalds/linux/blob/master/samples/bpf/bpf_load.h#L53
  *
- *
- *
- *
+ * We can bake some other logic in here, but just a thought.
  */
 int load_bpf_object_by_filename(char *path);
